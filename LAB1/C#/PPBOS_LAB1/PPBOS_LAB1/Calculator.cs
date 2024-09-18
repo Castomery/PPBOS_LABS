ef30c7a4c65760c73f2a157a9a94de00a8f4fefe
Vlad-Polygalov <vladpoligalov3@gmail.com>
2024-09-18 16:51:58 +0300

    Add Lab1 C#

---
+++ LAB1/C#/PPBOS_LAB1/PPBOS_LAB1/Calculator.cs
@@ -0,0 +1,63 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Diagnostics;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace PPBOS_LAB1
+{
+    internal class Calculator
+    {
+        private int[] arr;
+
+        public Calculator(int[] array)
+        {
+            this.arr = array;
+        }
+
+        public long GetSumByThreads(int countOfThreads)
+        {
+            Stopwatch stopwatch = new Stopwatch();
+            
+            long totalSum = 0;
+            long[] partialSum = new long[countOfThreads];
+            int arraySize = arr.Length;
+            int chunkSize = arraySize / countOfThreads;
+
+            Thread[] threads = new Thread[countOfThreads];
+
+            stopwatch.Start();
+            for (int i = 0; i < countOfThreads; i++)
+            {
+                int start = i * chunkSize;
+                int end = (i == countOfThreads - 1) ? arraySize : start + chunkSize;
+                int threadIndex = i;
+
+                threads[i] = new Thread(() => partialSum[threadIndex] = SumArrayPart(start, end));
+                threads[i].Start();
+            }
+
+            for (int i = 0; i < countOfThreads; i++)
+            {
+                threads[i].Join();
+                totalSum += partialSum[i];
+            }
+
+            stopwatch.Stop();
+            return totalSum;
+        }
+
+        private long SumArrayPart(int startIndex, int endIndex)
+        {
+            long localSum = 0;
+
+            for (int i = startIndex; i < endIndex; i++)
+            {
+                localSum += arr[i];
+            }
+
+            return localSum;
+        }
+    }
+}
---
+++ LAB1/C#/PPBOS_LAB1/PPBOS_LAB1/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.Diagnostics;
+
+namespace PPBOS_LAB1
+{
+    internal class Program
+    {
+        static int[] arr;
+        static int arrSize = 5000000;
+        static int countOfThreads = 5;
+        static void Main(string[] args)
+        {
+            arr = new int[arrSize];
+
+            for (int i = 0; i < arrSize; i++)
+            {
+                arr[i] = i;
+            }
+
+            Calculator calculator = new Calculator(arr);
+            Stopwatch timer = new Stopwatch();
+
+
+            
+            Console.WriteLine("One Thread");
+            timer.Start();
+            Console.WriteLine("Sum: " + calculator.GetSumByThreads(1));
+            timer.Stop();
+            Console.WriteLine("Time: " + timer.Elapsed.TotalMilliseconds);
+
+            timer.Reset();
+            Console.WriteLine("Multiple Threads");
+            timer.Start();
+            Console.WriteLine("Sum: " + calculator.GetSumByThreads(countOfThreads));
+            timer.Stop();
+            Console.WriteLine("Time: " + timer.Elapsed.TotalMilliseconds);
+
+            Console.ReadKey();
+        }
+    }
+}
